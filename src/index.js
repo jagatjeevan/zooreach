4e8c3cfaf106df9452115e946be1984c09b8d3f7
Jagatsah <jagatsah@in-Jagatsah.local>
2019-04-20 16:53:18 +0530

    Handle base path for github url

--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.scss';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.scss";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const baseUrl = window.location.hostname === 'jagatjeevan.github.io' ? 'zooreach' : '/';
+ReactDOM.render(
+  <Router basename={baseUrl}>
+    <Route path="/" component={App} />
+  </Router>,
+  document.getElementById("root")
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
